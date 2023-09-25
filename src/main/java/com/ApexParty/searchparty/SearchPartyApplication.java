32e9930ddd166d9590a259750f71127b546772de
hoho0161 <hoho0161@naver.com>
2023-09-25 18:34:29 +0900

    init: Initial commit
    프로젝트 생성

---
+++ src/main/java/com/ApexParty/searchparty/SearchPartyApplication.java
@@ -0,0 +1,13 @@
+package com.ApexParty.searchparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchPartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchPartyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ApexParty/searchparty/SearchPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApexParty.searchparty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchPartyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
