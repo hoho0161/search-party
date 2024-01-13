2d2c5fd5e768b5e5652d436b78d7ad69b7afb278
hoho0161 <hoho0161@naver.com>
2024-01-13 23:56:15 +0900

    feat: add Index.mustache, index.js, header,footer mustache
    메인페이지 템플릿(미완성), js코드(작동테스트) 추가, 머스테치 UTF-8 적용

---
+++ src/main/resources/static/js/app/index.js
@@ -0,0 +1,15 @@
+var main = {
+    init : function( ) {
+        var _this = this;
+        $('#selectTier').on('change', function() {
+            _this.changeTier();
+        });
+
+    },
+
+    changeTier : function () {
+        alert('JS테스트');
+    }
+};
+
+main.init();
